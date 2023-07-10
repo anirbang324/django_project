f3dadb21160deabe5a0e353be93c12704521018d
anirbang324 <anirbang324@gmail.com>
2023-07-10 23:53:39 +0530

    day-2

--- ecom/ecom/views.py
+++ ecom/ecom/views.py
@@ -1,5 +1,29 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .forms import ContactForm
 
 def home_page(request):
-    return render(request,"home.html",{})
+    context = {
+        'title': 'home Page',
+        'message': 'first page'
+    }
+    return render(request, "home.html", context)
+
+
+def about_page(request):
+    context = {
+        'title': 'About Page',
+        'message': 'learn more about us'
+    }
+    return render(request, "home.html", context)
+
+
+def contact_page(request):
+    contact_form = ContactForm()
+    context = {
+        'title': 'contact Page',
+        'message': 'Contact us',
+        'contact_form' : contact_form
+    }
+
+    return render(request, "contact/page.html", context)
